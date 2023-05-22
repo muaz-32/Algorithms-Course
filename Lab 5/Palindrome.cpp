343e9830d44adb709868f3e0622943ee0c35f995
muaz-32 <96006730+muaz-32@users.noreply.github.com>
2023-05-22 18:05:17 +0600

    Create Palindrome.cpp

---
+++ Lab 5/Palindrome.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int minInsertionsForPalindrome(const string& s, int left, int right, vector<vector<int>>& dp) {
+    if (left >= right) {
+        return 0;  
+    }
+    
+    if (dp[left][right] != -1) {
+        return dp[left][right];  
+    }
+
+    if (s[left] == s[right]) {
+        dp[left][right] = minInsertionsForPalindrome(s, left + 1, right - 1, dp);
+    } else {
+        dp[left][right] = min(minInsertionsForPalindrome(s, left + 1, right, dp),
+                               minInsertionsForPalindrome(s, left, right - 1, dp)) + 1;
+    }
+    
+    return dp[left][right];
+}
+
+int main() {
+    int N;
+    cin >> N;
+    
+    string s;
+    cin >> s;
+    
+    vector<vector<int>> dp(N, vector<int>(N, -1));  
+    
+    int minInsertions = minInsertionsForPalindrome(s, 0, N - 1, dp);
+    
+    cout << minInsertions << endl;
+    
+    return 0;
+}
